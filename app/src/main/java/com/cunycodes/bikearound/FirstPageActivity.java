bfccb9191a3d1fe1d3b5f472da85d1caf735f014
Francely Peralta <Francely@Francelys-MBP.home>
2017-04-15 13:22:51 -0400

    changed layout

--- app/src/main/java/com/cunycodes/bikearound/FirstPageActivity.java
+++ app/src/main/java/com/cunycodes/bikearound/FirstPageActivity.java
@@ -5,18 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class FirstPageActivity extends AppCompatActivity {
 
-    private Button btnLogin, btnCreate;
+    private ImageButton btnLogin, btnCreate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_page);
 
-        btnLogin = (Button) findViewById(R.id.btnLogin);
-        btnCreate = (Button) findViewById(R.id.btnCreateAccount);
+        btnLogin = (ImageButton) findViewById(R.id.btnLogin);
+        btnCreate = (ImageButton) findViewById(R.id.btnCreateAccount);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
